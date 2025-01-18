6fa5e8a62e2dba83189da434dfc3a6f710a81a25
조영재 <jyj1143@gmail.com>
2025-01-19 08:21:23 +0900

    feat: #2 - common-jwt추가
    - JWT 토큰 생성, 관리하는 서비스 클래스 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,36 +1,33 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    kotlin("jvm") version "1.9.25"
-    kotlin("plugin.spring") version "1.9.25"
+    kotlin("jvm") version "1.9.24"
+    kotlin("plugin.spring") version "1.9.24"
+    kotlin("plugin.jpa") version "1.7.20"
+
     id("org.springframework.boot") version "3.4.1"
     id("io.spring.dependency-management") version "1.1.7"
 }
 
-group = "com.giunne"
-version = "0.0.1-SNAPSHOT"
+allprojects {
+    group = "com.giunne"
+    version = "0.0.1-SNAPSHOT"
 
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
+    repositories {
+        mavenCentral()
     }
 }
 
-repositories {
-    mavenCentral()
-}
 
-val nonDependenciesProjects = listOf("commons")
+val nonDependenciesProjects = listOf("commons", "giunne-service")
 configure(subprojects.filter { it.name !in nonDependenciesProjects }) {
 
     apply(plugin = "org.springframework.boot") // Spring Boot 플러그인 적용
     apply(plugin = "io.spring.dependency-management")  // Spring 의존성 관리 플러그인 적용
     apply(plugin = "kotlin")  // Kotlin 플러그인 적용
     apply(plugin = "kotlin-spring") // Kotlin-Spring 플러그인 적용(Spring 어노테이션을 Kotlin 클래스에 자동으로 열어주는 역할)
-    apply(plugin = "kotlin-jpa") // Kotlin-JPA 플러그인을 적용(Kotlin 엔티티 클래스에 기본 생성자를 자동으로 생성해주는 역할)
 
     dependencies {
-
         implementation("org.jetbrains.kotlin:kotlin-reflect") // Kotlin 리플렉션 라이브러리
         implementation("org.springframework.boot:spring-boot-starter") // Spring Boot Starter
 
@@ -41,10 +38,7 @@ configure(subprojects.filter { it.name !in nonDependenciesProjects }) {
     }
 
     kotlin {
-        compilerOptions {
-            freeCompilerArgs.addAll("-Xjsr305=strict")
-            jvmTarget.set(JvmTarget.JVM_21)
-        }
+        jvmToolchain(21)
     }
 
     tasks.withType<Test> {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,8 @@ rootProject.name = "giunne"
 
 include("commons")
 include("commons:common-web")
+include("commons:common-jwt")
+include("commons:common-util")
+
+include("giunne-service")
+include("giunne-service:giunne-auth")
