c8ec2853c6be2cc6cec58694bea3a50b9e65acc4
조영재 <jyj1143@plateer.com>
2025-03-03 17:33:45 +0900

    feat: #57 로드맵 검색 API추가: 코드 속성 추가
    - 로드맵 운동설명 추가
    - 현재 진행 상태 추가
    - 경험치 추가
    - 포인트 추가
    - 유튜브URL 추가
    - 현재 인증 횟수 추가
    - 필요 인증 횟수 추가
    - 다음 단계 추가

---
+++ quest-service/src/main/java/com/giunne/questservice/domain/course/domain/type/NeedApproveCnt.java
@@ -0,0 +1,29 @@
+package com.giunne.questservice.domain.course.domain.type;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 보상 경험치
+ */
+
+@Getter
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class NeedApproveCnt {
+    @Column(name = "need_approve_cnt", nullable = false)
+    private Integer value;
+
+    private NeedApproveCnt(final Integer value) {
+        this.value = value;
+    }
+
+    public static NeedApproveCnt from(final Integer value) {
+        return new NeedApproveCnt(value);
+    }
+    
+}
